66502341c248092344103b967c24ebc02607e4a1
msciborski <michal.sciborski@student.put.poznan.pl>
2017-09-12 17:55:58 +0200

    BookMeContext- DbContext

---
+++ BookMe.Domain/Concrete/BookMeContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookMe.Domain.Entities;
+
+namespace BookMe.Domain.Concrete {
+    public class BookMeContext : DbContext {
+
+        public BookMeContext() : base("BookMeDb") { }
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+    }
+}
