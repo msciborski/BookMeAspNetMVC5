ebfd99838fbe2945f18752e8c34d218ed5a12d0b
msciborski <michal.sciborski@student.put.poznan.pl>
2017-10-04 00:14:09 +0200

    Adding jquery, jquery-ui, Updating bootstrap, webgres Creating bundles

---
+++ BookMe.WebUI/App_Start/BundleConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace BookMe.WebUI.App_Start {
+    public class BundleConfig {
+        public static void RegisterBundles(BundleCollection bundles){
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                "~/Scripts/jquery-ui-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/moment").Include(
+                "~/Scripts/moment.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap-datetimepicker").Include(
+                "~/Scripts/bootstrap-datetimepicker.js"));
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
+                "~/Content/bootstrap.css"));
+            bundles.Add(new StyleBundle("~/Content/bootstrap-datetimepicker").Include(
+                "~/Content/bootstrap-datetimepicker.css"));
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                "~/Content/themes/base/all.css"
+                ));
+            BundleTable.EnableOptimizations = false;
+        }
+    }
+}
--- BookMe.WebUI/Global.asax.cs
+++ BookMe.WebUI/Global.asax.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
+using BookMe.WebUI.App_Start;
 
 namespace BookMe.WebUI
 {
@@ -13,6 +15,7 @@ namespace BookMe.WebUI
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
